120b6c1caf7caf692881475bfff760140415344b
Scott Provence <scott.provence@yahoo.com>
2021-07-03 21:43:40 -0400

    Add model for Transactions and basic server

---
+++ models/Transaction.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const transactionSchema = new Schema(
+  {
+    transactionType: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    subCategory: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    data: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Transaction = mongooose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
